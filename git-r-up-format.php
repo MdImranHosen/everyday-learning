45d16144489b5d0444b7e3d8d52745ac5052edbe
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2019-10-06 19:21:01 +0600

    git re_p upload way

---
+++ git-r-up-format.php
@@ -0,0 +1,12 @@
+<?php
+cd "url"
+git init
+git remote add or origin <gitl link>
+git remote -v
+git add .
+git config user.name "Md Imran Hosen"
+git config user.email "imranhosen5912@gmail.com"
+git commit -am "first commit"
+git push origin master
+
+?>
