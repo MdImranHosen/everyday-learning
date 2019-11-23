6b8529e6d9e4e64e389bf614656c618e42b5f84a
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2019-11-23 14:52:06 +0600

    Add change script

--- language.php
+++ language.php
@@ -42,6 +42,37 @@ if(isset($_COOKIE["LANGUAGE"]) AND $_COOKIE["LANGUAGE"]=="en") {
 }
 ?>
 
+<script type="text/javascript">
+function change_language(ext){
+ //alert(ext);
+setCookie("LANGUAGE", ext, 20);
+  location.reload();
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+</script>
+
 ///////////  demo/text/menu_bn.json  //////////////////////
 
 [
