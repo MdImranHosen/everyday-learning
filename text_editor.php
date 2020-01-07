d404f958c5fdb721199400710ebd1c97e51d3280
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2020-01-07 10:02:05 +0600

    Textbox

---
+++ text_editor.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+ <title>Textbox Editor</title>
+</head>
+<body>
+   <!-- 1. Textbox Editor Start -->
+   <h1>Textbox Editor</h1>
+   <div>
+      <h2><a href="https://textbox.io">Textbox site</a></h2>
+      <p>
+       Download sdk File then use
+       # textboxio.js
+       # <strong>Folders : </strong><b> resources </b>
+      </p>
+   </div>
+   <script>textboxio.replace('textarea');</script>
+   <!-- End Textbox Editor -->
+</body>
+</html>
