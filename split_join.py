288b931bbc04e62b4c2714ef4ffbbe6bf50f90b9
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2020-12-02 21:25:25 +0600

    Mutations

--- split_join.py
+++ split_join.py
@@ -16,3 +16,13 @@ if __name__ == '__main__':
     b = input()
     result = name_me(a,b)
     print(result)
+    
+    #//////////////////////////Mutations String Python/////////////////
+    
+def imranHosen(st, p, c):
+    return  st[:p]+c+st[p+1:]
+if __name__ == '__main__':
+    imran = input()
+    i, m = input().split()
+    result =  imranHosen(imran, int(i), m)
+    print(result)
