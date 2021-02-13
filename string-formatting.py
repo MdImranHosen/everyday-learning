7c2e016319d932a1a0d2ffe1074efa1fce9e6fe8
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-02-13 13:51:18 +0600

    string formatting

---
+++ string-formatting.py
@@ -0,0 +1,7 @@
+def print_formatted(number):
+   l=len("{0:b}".format(number))
+   for i in range(1,number+1):
+       print("{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i,w=l))
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
