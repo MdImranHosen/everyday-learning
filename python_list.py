766a7e3385f9b1f75dbcfaccca10f4ffd005e1e4
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-03-11 09:43:14 +0600

    datetime seconds

--- python_list.py
+++ python_list.py
@@ -179,3 +179,17 @@ m,d,y= map(int, input().split(" "))
 
 w = calendar.weekday(y,m,d)
 print(weekdays[w])
+
+#//////////////////// ***************** datetime by seconds ******************* \\\\\\\\\\\\\\\\\\\\\\\\
+from  datetime import datetime
+
+def time_delta(t1, t2):
+    time_format = "%a %d %b %Y %H:%M:%S %z"
+    time1 = datetime.strptime(t1, time_format)
+    time2 = datetime.strptime(t2, time_format)
+    return int(abs(time1 - time2).total_seconds())
+for i in range(int(input())):
+    t1 = input()
+    t2 = input()
+    print(time_delta(t1, t2))
+
