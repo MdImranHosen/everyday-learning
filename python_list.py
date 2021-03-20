9af2403671cdc3725bba096da1badbe728ffd615
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-03-20 14:27:57 +0600

    Collections deque

--- python_list.py
+++ python_list.py
@@ -250,3 +250,19 @@ for i in range(int(input())):
     d[input()] += 1
 print(len(d.items()))
 print(*d.values())
+
+# ////////////// ************** Collections.deque() ***************\\\\\\\\\\\\\\\
+from collections import *
+d=deque()
+for i in range(int(input())):
+    s= input().split()
+    if s[0]=="append":
+        d.append(s[1])
+    elif s[0]=="pop":
+        d.pop()
+    elif s[0]=="appendleft":
+        d.appendleft(s[1])
+    else:
+        d.popleft()
+for i in d:
+    print(i, end=" ")
