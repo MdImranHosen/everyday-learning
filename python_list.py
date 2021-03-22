50e7479be7903bd7ace8ee4bd29649db5cb52562
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-03-22 10:40:05 +0600

    Symmetric Difference

--- python_list.py
+++ python_list.py
@@ -266,3 +266,19 @@ for i in range(int(input())):
         d.popleft()
 for i in d:
     print(i, end=" ")
+# ////////////////// *********** Symmetric Difference ********************* \\\\\\\\\\\\\\\\\
+n = int(input())
+s1 = set(map(int, input().split()))
+m = int(input())
+s2 = set(map(int, input().split()))
+s3 = s1.union(s2)
+
+l = []
+for i in s3:
+    if i in s1 and i in s2:
+        pass
+    else:
+        l.append(i)
+l.sort()
+for i in l:
+    print(i)
