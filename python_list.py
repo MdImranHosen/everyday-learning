cad8846a15e94ebe61025093baff8699f1b82469
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-03-23 09:50:26 +0600

    pop remove

--- python_list.py
+++ python_list.py
@@ -307,3 +307,20 @@ sum=0
 for j in s:
     sum=sum+j
 print(sum)
+
+#////////
+
+m = int(input())
+s = set(map(int, input().split()))
+n = int(input())
+for i in range(n):
+    commands = input().split()
+    if len(commands) > 1:
+        e = int(commands[1])
+    if commands[0] == "pop":
+        s.pop()
+    if commands[0] == "remove":
+        s.remove(e)
+    if commands[0] == "discard":
+        s.discard(e)
+print(sum(s))
