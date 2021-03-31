ab9376a9ed9a8fab1ec000241d5e4330451b28a2
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-03-31 10:01:16 +0600

    Piling Up

--- python_list.py
+++ python_list.py
@@ -378,3 +378,14 @@ for i in ans:
     count = count+1
 print(count)
 
+#///////////////////// *********************** Piling Up *********************************\\\\\\\\\\\\\\\\\\\\\\\
+for t in range(int(input())):
+    l = int(input())
+    sides = list(map(int, input().split()))
+    i = 0
+    while i < l - 1 and sides[i] >= sides[i + 1]:
+        i += 1
+    while i < l - 1 and sides[i] <= sides[i + 1]:
+        i += 1
+    print('Yes' if i == l - 1 else 'No')
+
