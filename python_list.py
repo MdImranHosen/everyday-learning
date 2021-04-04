8d513b1e47cf65d1c1c404594259120697ce4d68
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-04-04 16:52:27 +0600

    maximize it

--- python_list.py
+++ python_list.py
@@ -434,3 +434,17 @@ c_list = list(combinations(arr, m))
 
 a_list = [i for i in c_list if 'a' in i]
 print(len(a_list) / len(c_list))
+
+#/////////////////// ********************************* Maximize It  ******************************* \\\\\\\\\\\\\\\\\\
+from itertools import product
+
+k, m = map(int, input().split())
+
+array = []
+for _ in range(k):
+    array.append(list(map(int, input().split()))[1:])
+
+result = 0
+for combination in product(*array):
+    result = max(sum([x * x for x in combination]) % m, result)
+print(result)
