a931ead5793170ec84c00dd51bdcea41fd343de4
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-04-24 13:33:56 +0600

    Letters character count

--- python_list.py
+++ python_list.py
@@ -579,4 +579,14 @@ if __name__ == '__main__':
     y = (c - b).cross( d - c)
     angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
     print("%.2f" % math.degrees(angle))
-
+#////////////************ Letters  character count ****** \\\\\\\\\
+text = input()
+dict = {}
+for char in text:
+    if char in dict:
+       dict[char] += 1 
+     
+    else: 
+       dict[char] = 1
+       
+print(dict)
