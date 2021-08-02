7bbcdfff05aafd0ec9dc1deb58b4d669e83b9a8a
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-08-02 10:00:04 +0600

    any or all

--- python_list.py
+++ python_list.py
@@ -604,4 +604,11 @@ for i in zip(*mxs):
 #//////////////// ****************** Python Evaluation HackerRank ************************** \\\\\\\\\\\\\\\\\\\\\
 e = input()
 eval(e)
+
+#/////////// ************************** Python Any or All ********************* \\\\\\\\\\\\\\\\\\\
+if __name__ == "__main__":
+    num_cnt = int(input().strip())
+    arr = list(input().strip().split())
+    print(all([all([int(x) > 0 for x in arr ]), any([x == x[::-1] for x in arr])]))
+    
     
