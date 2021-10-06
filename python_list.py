2429dc8fc2d6dc1de651d46e990bb71b2ead0463
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-10-06 09:07:11 +0600

    set discard

--- python_list.py
+++ python_list.py
@@ -687,3 +687,17 @@ if __name__ == '__main__':
         imran.append(Fraction(*map(int, input().split())))
     result = dfun(imran)
     print(*result)
+    
+#/////////////// **************************** Set discard() remove() and pop() ****************************** \\\\\\\\\\\\\\\\\\\\\\\\\
+n = int(input())
+s = set(map(int, input().split()))
+num = int(input())
+for i in range(num):
+    ip = input().split()
+    if ip[0] == "remove":
+        s.remove(int(ip[1]))
+    elif ip[0] == "discard":
+        s.discard(int(ip[1]))
+    else:
+        s.pop()
+print(sum(list(s)))
