1e5fea5e86db3a81410086a57e43b0119ce114f0
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-04 08:54:18 +0600

    start and end py

--- python_list.py
+++ python_list.py
@@ -701,3 +701,16 @@ for i in range(num):
     else:
         s.pop()
 print(sum(list(s)))
+#//////////////////////// ***************************** ********************************* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+import re
+S, k = input(), input()
+
+matches = re.finditer(r'(?=(' + k + '))', S)
+
+anymatch = False
+for match in matches:
+    anymatch = True
+    print((match.start(1), match.end(1) - 1))
+
+if anymatch == False:
+    print((-1, -1))
