383be41e6274fdc34780478e143fa7d212f22599
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-10 10:07:23 +0600

    CYs 5th commit

--- cyber_security.php
+++ cyber_security.php
@@ -1,11 +1,42 @@
-<h1> /////************  Admin Panel Hack – SQL Injection **************\\\\\\ </h1>
+<h1> /////************ 07-11-2021  Admin Panel Hack – SQL Injection **************\\\\\\ </h1>
 <p>
-  Search
 intitle:"index"of"admin"site:.in
+
 inurl:/admin/index.php / login.php/ user.php
 1.	Search Google -> Admin/index.php
 2.	Open Website Url: www. Like . com/ admin
 3.	Username: 1 'or' 1 '=' 1
 4.	Password:  1 'or' 1 '=' 1
+</p>
 
+<h1> /////************ 07-11-2021  nmap use by IP/ Port **************\\\\\\ </h1>
+<p>
+	1. nmap install
+	2. scan web address to IP/ Port open list
+	3. fishing url make and get password
+
+</p>
+<h1> /////************  Key Logger **************\\\\\\ </h1>
+<p>
+   1. windows => Key Logger Install "www. spyrix. com/" Advance: "Ardamax Keylogger"
+   2. Android => key logger "androidkeylogger . com"
+   3. Case  Study Digital Footprint
+</p>
+<h1> /////************  Encryption **************\\\\\\ </h1>
+<p>
+	1. Encryption Password
+	2. md5, sha2, crp32
+</p>
+<h1> /////************  Web Application Security Scanner **************\\\\\\ </h1>
+<p>
+	1. Encryption Password
+	2. md5, sha2, crp32
+</p>
+<h1>/////************ 10-11-2021  Steganography Data Hiding Technique ************\\\\\\ </h1>
+<p>
+	1. Steganography Data Hiding Technique
+	2. www. pelock. com/products/steganography-online-codec
+	3. www. pelock. com/products/string-encrypt
+	4. QuickStego Software
+	5. OpenStego Software
 </p>
