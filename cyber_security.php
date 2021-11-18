cca4bae92800f4097682b1ada3c43ac6400c6d16
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-18 20:33:06 +0600

    CY 1 by 1

--- cyber_security.php
+++ cyber_security.php
@@ -1,77 +1,272 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cyber Security - Md. Imran Hosen </title>
+	<style type="text/css">
+		*{
+			font-family: serif;
+		}
+		.container{
+			  width: 90%;
+			  padding-right: $padding-x;
+			  padding-left: $padding-x;
+			  margin-right: auto;
+			  margin-left: auto;
+		}
+		.p-0{
+			padding: 0 !important;
+		}
+		.m-0{
+			margin: 0 !important;
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+	
+
+<h1> MD. IMRAN HOSEN </h1>
+<h2> Web Application Developer at University of Dhaka </h2>
+<a href="www.github.com/MdImranHosen">Github/MdImranHosen</a>
+<br>
+<hr>
+
+<h1> **** Steps To Become A Hacker ***** 18-11-2021 ***** </h1>
+<ul>
+	<li>Linux</li>
+	<li>Language Free Programming </li>
+	<li>Become anonymous </li>
+	<li>Networking Concepts </li>
+	<li>Explore the Hidden web </li>
+	<li>Explore Vulnerabilities </li>
+	<li>Practicing & test various tools </li>
+	<li>Join Discussion and Meet the Hackers </li>
+	<li>Certified Ethical Hacker (CEH)</li>	
+	<li> & Offensive Security Certified Professional (OSCP)</li>
+</ul>
+
+<hr>
+
 <h1> /////************ 07-11-2021  Admin Panel Hack – SQL Injection **************\\\\\\ </h1>
 <p>
 intitle:"index"of"admin"site:.in
-
+<br class="p-0 m-0">
 inurl:/admin/index.php / login.php/ user.php
-1.	Search Google -> Admin/index.php
-2.	Open Website Url: www. Like . com/ admin
-3.	Username: 1 'or' 1 '=' 1
-4.	Password:  1 'or' 1 '=' 1
+<br>
+<ul>
+	<li> Search Google -> Admin/index.php </li>
+	<li> Open Website Url: www. Like . com/ admin </li>
+	<li> Username: 1 'or' 1 '=' 1 </li>
+	<li> Password:  1 'or' 1 '=' 1 </li>
+</ul>
 </p>
+<hr>
 
 <h1> /////************ 07-11-2021  nmap use by IP/ Port **************\\\\\\ </h1>
 <p>
-	1. nmap install
-	2. scan web address to IP/ Port open list
-	3. fishing url make and get password
-
+  <ul>
+	<li> nmap install </li>
+	<li> scan web address to IP/ Port open list </li>
+	<li> fishing url make and get password </li>
+  </ul>
 </p>
+<hr>
+
 <h1> /////************  Key Logger **************\\\\\\ </h1>
 <p>
-   1. windows => Key Logger Install "www. spyrix. com/" Advance: "Ardamax Keylogger"
-   2. Android => key logger "androidkeylogger . com"
-   3. Case  Study Digital Footprint
+	<ul>	
+     <li> Windows => Key Logger Install "www. spyrix. com/" Advance: "Ardamax Keylogger" </li>
+     <li> Android => key logger "androidkeylogger . com" </li>
+     <li> Case  Study Digital Footprint </li>
+   </ul>
 </p>
+<hr>
+
 <h1> /////************  Encryption **************\\\\\\ </h1>
 <p>
-	1. Encryption Password
-	2. md5, sha2, crp32
+	<ul>
+	  <li> Encryption Password </li>
+	  <li> md5, sha2, crp32 </li>
+	</ul>
 </p>
-<h1> /////************  Web Application Security Scanner **************\\\\\\ </h1>
-<p>
-	1. Encryption Password
-	2. md5, sha2, crp32
-</p>
+<hr>
+
 <h1>/////************ 10-11-2021  Steganography Data Hiding Technique ************\\\\\\ </h1>
 <p>
-	1. Steganography Data Hiding Technique
-	2. www. pelock. com/products/steganography-online-codec
-	3. www. pelock. com/products/string-encrypt
-	4. QuickStego Software
-	5. OpenStego Software
+	<ul>
+	   <li> Steganography Data Hiding Technique </li>
+	   <li> www. pelock. com/products/steganography-online-codec </li>
+	   <li> www. pelock. com/products/string-encrypt </li>
+	   <li> QuickStego Software </li>
+	   <li> OpenStego Software </li>
+	   <li> What is Steganography? </li>
+	</ul>
 </p>
+<hr>
+
+
 <h1>/////************ 13-11-2021  DDoS Attact ************\\\\\\ </h1>
 <p>
-	1. Distributed Denial of Service (DDoS) Attact
-	  ***Populer Tools
-	2. LOIC (Low Orbit Ion Canon)
-	3. HOIC (High Orbit Ion Canon)
-	4. Ping of Death
-	5. First CMD (Command Prompt) Open and type now (for website IP show)
-	6. => nslookup site name
-	   =>ping (site IP) -t -l 65500 -n 1000000 -w 0.00001
-	   -t = pings the specified host until stopped
-	   -l = send buffer size
-	   -n 10000000 = Count Number of echo requests to send
-	   -w 0.00001 = Timeout in milliseconds to wait for each reply. 
-	7. isitdownrightnow . com check for website Down status ..
+	<ul>
+	<li> Distributed Denial of Service (DDoS) Attact</li>
+	  <h3> *** Populer Tools ** </h3>
+	<li> LOIC (Low Orbit Ion Canon)</li>
+	<li> HOIC (High Orbit Ion Canon)</li>
+	<li> Ping of Death</li>
+	<li> First CMD (Command Prompt) Open and type now (for website IP show)</li>
+	<li> => nslookup site name 
+	  <ul>
+	   <li> =>ping (site IP) -t -l 65500 -n 1000000 -w 0.00001</li>
+	   <li> -t = pings the specified host until stopped</li>
+	   <li> -l = send buffer size</li>
+	   <li> -n 10000000 = Count Number of echo requests to send</li>
+	   <li> -w 0.00001 = Timeout in milliseconds to wait for each reply. </li>
+	  </ul>
+	</li>
+	<li> isitdownrightnow . com check for website Down status ..</li>
+	</ul>
 </p>
+<hr>
 
 <h1>/////************ 17-11-2021  Dark Web / Deep Web ************\\\\\\ </h1>
 <p>
-	1. First Instal Tor Browser 
-	2. Search thehidden wiki . org
-	3. web site extension . onion
-	*****
-	1. Bug Bounging
-	
+	<ul>
+		<li> First Instal Tor Browser  </li>
+		<li> Search thehidden wiki . org </li>
+		<li> web site extension . onion </li>
+		<li> ***** </li>
+		<li> Bug Bounging </li>
+	</ul>
 </p>
+<hr>
+<br>
 
 <h1>/////************ 17-11-2021  Havij Download – Advanced SQL Injection Tool ************\\\\\\ </h1>
 <p>
-	1. Havij Download – Advanced SQL Injection Tool
-
-   2. You can download Havij v1.12 Free Edition here:
-
-   3. Password is darknet123
+  <ul>
+	<li> Havij Download – Advanced SQL Injection Tool </li>
+   <li> You can download Havij v1.12 Free Edition here: </li>
+   <li> Password is darknet123 </li>
+  </ul>
+   https:// www. darknet. org. uk/2010/09/
+   <br class="p-0 m-0">
+   havij-advanced-automated-sql-injection-tool/#google_vignette
 </p>
+<hr>
+<br>
+
+<h1>/////************ 18-11-2021  Malware, Ransomware Attack ************\\\\\\ </h1>
+<div>
+	<ul>
+	 <li> Track PC Activities </li>
+
+    <li> Using Network protocol analyzer: Wireshark </li>
+   </ul>
+   <br>
+
+   <h3> ** Hosts Blocked ** </h3>
+   <ul>
+     <li> C:\Windows\System32\drivers\etc -> hosts  </li>
+     <li> IP Block </li>
+   </ul>
+   <br>
+
+   <h3> ** OS Rootkit **  </h3>
+   <p> => A Rootkit is a clandestine computer program designed to provide continued privileged access to a computer while actively hiding its presence.  </p>
+  <h4> # Rootkit list </h4>
+  <ul>
+    <li> => Lane Davis and Steven Dake </li>
+    <li> => NTRootkit </li>
+    <li> => HackerDefender </li>
+    <li> => Machiavelli </li>
+    <li> => Greek wiretapping </li>
+    <li> => Zeus </li>
+    <li> => Stuxnet </li>
+   </ul>
+   <h3>  ** Pastejacking ** </h3>
+
+  <p> => Pastejacking is a very powerful technique for exploiting victims. As the Command copied by the victim is changed and any command which may be harmful for the victims information, credentials, and private data may be at a risk. </p>
+
+  <h3> ** Mariana Web ** </h3>
+  <ul>
+    <li> => In the mythology of the dark web Marianas Web is at Level 5. </li>
+    <li> => The Stories of multiple layers of the dark web, with each layer getting a bit more dark and mysterious, sound a lot like a fictional depiction of the layers of hell dating back to the 14th-century. </li>
+   </ul>
+</div>
+<hr>
+<br>
+
+<div>
+<h1>Crypto Currency</h1>
+
+<p>
+	=> Digital Currency that can be used to buy goods and services, but uses an online ledger with strong cryptography to secure online transactions.
+</p>
+<ul>
+	<li> => Bitcoin </li>
+	<li> => Ethereum </li>
+	<li> => Litecoin </li>
+	<li> => Ripple </li>
+</ul>
+<p>
+	More than 10,000 different cryptocurrencies are traded publicly, according to CoinMarketCap . com
+</p>
+<h3> Cryptocurrency Brokers </h3>
+<ul>
+	<li>Coinbase </li>
+	<li>eToro</li>
+	<li>Gemini </li>
+	<li>Robinhood </li>
+	<li>TradeStation </li>
+	<li>Webull </li>
+</ul>
+<h3> Cryptocurrency Features </h3>
+<ul>
+	<li>Hashing and Digital Signatures </li>
+	<li>Anonymity </li>
+	<li>No intermediary of supervisory bodies </li>
+	<li>Security </li>
+	<li>No Centralization </li>
+	<li>Sending Cryptocurrencies </li>
+	<li>Irreversable transactions </li>
+	<li>Fast development </li>
+</ul>
+
+</div>
+<hr>
+<br>
+<div>
+	<h1>////////******* Wifi Hack ********\\\\\\\\*** 18-11-2021 ***////</h1>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<hr>
+<br>
+
+
+
+
+
+
+
+
+<!-- DEMO TEM
+
+<div>
+	<h1>////////******* ********\\\\\\\\*** ***////</h1>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<hr>
+<br> 
+
+-->
+
+/////////////////////////////////////////////// END//////////////////////////////////////////////////////
+</div>
+</body>
+</html>
