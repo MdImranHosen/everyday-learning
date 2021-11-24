580fdeb90f18651a26875402990a8fbfcdb4d537
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-24 20:09:36 +0600

    msc q commit1

---
+++ msc-in-it-admission.php
@@ -0,0 +1,413 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cyber Security - Md. Imran Hosen </title>
+	<style type="text/css">
+		*{
+			font-family: serif;
+		}
+		.container{
+			  width: 90%;
+			  padding-right: $padding-x;
+			  padding-left: $padding-x;
+			  margin-right: auto;
+			  margin-left: auto;
+		}
+		.p-0{
+			padding: 0 !important;
+		}
+		.m-0{
+			margin: 0 !important;
+		}
+		.which-100{
+			/*width: 20%;*/
+			clear: both;
+		}
+		.w_lr{
+			
+		}
+		.w_lr ul li{
+			list-style: inline;
+		}
+		h3{ margin:0px; }
+	</style>
+</head>
+<body>
+<div class="container">
+
+<h1> MD. IMRAN HOSEN </h1>
+<h2> Web Application Developer at University of Dhaka </h2>
+<a href="www.github.com/MdImranHosen">Github/MdImranHosen</a>
+<hr>
+<br>
+<div>
+	<h1>////////******* Firmware & MiddleWare ********\\\\\\\\*** 20-11-2021 ***////</h1>
+  
+	<div class="which-100">
+		<h3> => Which one is a Volatile memory ?</h3>
+		<div class="w_lr">
+			<ul>
+				<li>RAM</li>
+				<li>Hard disk</li>
+				<li>CD</li>
+				<li>ROM</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: RAM </strong></p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => A “glue” between client and server parts of application ?</h3>
+		<div class="w_lr">
+			<ul>
+				<li>Middleware</li>
+				<li>Firmware</li>
+				<li>Package</li>
+				<li>System Software</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Middleware </strong></p>
+		<p>
+			Explanation: A middleware acts as a glue between client and server parts of application. It provides programming abstraction which means hiding all the relevant details.
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => Middleware has enabled the production of various types of smart machines having microprocessor chips with embedded software. ?</h3>
+		<div class="w_lr">
+			<ul>
+				<li>True</li>
+				<li>False</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: False </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: The statement is false. Firmware is designed for this purpose and not middleware. A middleware is for providing abstraction in programming.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => MOM stands for? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Message oriented middleware</li>
+				<li>Mails oriented middleware</li>
+				<li>Middleware of messages</li>
+				<li>Main object middleware</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Message oriented middleware </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: MOM is message-oriented middleware. It is basically responsible for sending and receiving messages across distributed systems.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => Storage of firmware is ___________ ? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Cache Memory</li>
+				<li>RAM</li>
+				<li>External</li>
+				<li>ROM</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: ROM </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: Firmware is stored in ROM which is the read only memory. Firmware basically acts as a link between the hardware and the system.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => DNS stands for? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Domain Name System</li>
+				<li>Direct Name System</li>
+				<li>Direct Network System</li>
+				<li>Domain Network System</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Domain Name System </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: DNS stands for Domain Name System. Domain name system is the way in which the internet domain names are traced and then translated into IP addresses.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => A software that lies between the OS and the applications running on it. </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Firmware</li>
+				<li>Middleware</li>
+				<li>Utility Software</li>
+				<li>Application Software</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Middleware </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: It is called as middleware. Middleware enables the interaction between the Operating System and the applications running on it.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => A type of middleware that allows for between the built-in applications and the real-time OS? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Firmware</li>
+				<li>Database middleware</li>
+				<li>Portals</li>
+				<li>Embedded Middleware</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Embedded Middleware </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: It is called the embedded middleware since it activates the communication link between the built-in applications and the real time operating system.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => What is the other name for object middleware? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Object request interface</li>
+				<li>Object enabled interface</li>
+				<li>Object Request broker</li>
+				<li>Object enabled broker</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: Object Request broker </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: It is also called as object request broker. It gives the applications ability to send the objects and receive services through an object.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => The _______ calls certain procedures on remote systems and is used to perform synchronous or asynchronous interactions between systems. </h3>
+		<div class="w_lr">
+			<ul>
+				<li>Procedure</li>
+				<li>RPC</li>
+				<li>Message Oriented</li>
+				<li>DB</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: RPC </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: It is called the RPC or the Remote Procedure Call. The functioning is to call certain procedures on remote applications. This is generally utilized in a software application.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	
+</div>
+<hr>
+<br>
+
+<div>
+	<h1>////////******* The Binary Number System ********\\\\\\\\*** 24-11-2021 ***////</h1>
+  
+	<div class="which-100">
+		<h3> => Which of the following is not a positional number system? ?</h3>
+		<div class="w_lr">
+			<ul>
+				<li>Roman Number System</li>
+				<li>Octal Number System</li>
+				<li>Binary Number System</li>
+				<li>RHexadecimal Number System</li>
+			</ul>
+		</div>
+		<p class="p-0 m-0"> <strong> Answer: Roman Number System </strong></p>
+		<p class="p-0 m-0">
+			Explanation: The Roman number system isn’t a positional number system since it uses symbols to represent numbers. The octal number system uses digits from 0-7, the binary number system uses digits from 0-1 whereas, the hexadecimal number system uses digits from 0-15.
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => The value of radix in binary number system is _____________ </h3>
+		<div class="w_lr">
+			<ul>
+				<li>2</li>
+				<li>8</li>
+				<li>10</li>
+				<li>1</li>
+			</ul>
+		</div>
+		<p class="p-0 m-0"> <strong> Answer: 2 </strong></p>
+		<p class="p-0 m-0">
+			Explanation: In a binary number system, the value of base or radix is 2. The binary system uses only two digits for the representation of numbers, therefore its base id has chosen to be 2.
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => The binary equivalent of the decimal number 10 is __________ </h3>
+		<div class="w_lr">
+			<ul>
+				<li>0010</li>
+				<li>10</li>
+				<li>1010</li>
+				<li>010</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: 1010 </strong></p>
+		<p class="p-0 m-0">
+		  planation: To get the binary equivalent of any number, we need to divide the number by 2 and obtain the remainders as :	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => A computer language that is written in binary codes only is ____ </h3>
+		<div class="w_lr">
+			<ul>
+				<li>machine language</li>
+				<li>C</li>
+				<li>C#</li>
+				<li>pascal</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: machine language </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: Machine Language is written in binary codes only. It can be easily understood by the computer and is very difficult for us to understand. A machine language, unlike other languages, requires no translators or interpreters.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+
+	<div class="which-100">
+		<h3> => The octal equivalent of 1100101.001010 is ______ </h3>
+		<div class="w_lr">
+			<ul>
+				<li>624.12</li>
+				<li>145.12</li>
+				<li>154.12</li>
+				<li>145.21</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: 145.12 </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: The octal equivalent is obtained by grouping the numbers into three, from right to left before decimal and from right to left after the decimal place.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => The input hexadecimal representation of 1110 is ______ </h3>
+		<div class="w_lr">
+			<ul>
+				<li>0111</li>
+				<li>E</li>
+				<li>15</li>
+				<li>14</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: E </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: In hexadecimal number system, 1110 = 15, which is represented by the alphabet E.
+			Some representations are:
+			A 10
+			B 11
+			C 12
+			D 13
+			E 14
+			F 15.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => A bit in a computer terminology means either 0 or 1. </h3>
+		<div class="w_lr">
+			<ul>
+				<li>True</li>
+				<li>False</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: True </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: A bit stands for a binary digit. A binary digit can have only two digits i.e. 0 or 1. A binary number consisting of n-bits is called an n-bit number.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => Convert the binary equivalent 10101 to its decimal equivalent. </h3>
+		<div class="w_lr">
+			<ul>
+				<li>21</li>
+				<li>12</li>
+				<li>22</li>
+				<li>31</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: 21 </strong></p>
+		<p class="p-0 m-0">
+		  xplanation: To convert a binary number to its decimal equivalent follow these steps :
+			24 * 1 + 23 * 0 + 22 *1 + 21 * 0 + 20 * 1 = 21.
+			Therefore, the answer is 21.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => Which of the following is not a binary number? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>1111</li>
+				<li>101</li>
+				<li>11E</li>
+				<li>000</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: 11E </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: A binary number can have only two possible digits, 0 and 1. In the third option, there is an alphabet E present which makes it an invalid binary number. Alphabets are only allowed in the hexadecimal number system.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	<div class="which-100">
+		<h3> => Which of the following is the correct representation of a binary number? </h3>
+		<div class="w_lr">
+			<ul>
+				<li>(124)2</li>
+				<li>1110</li>
+				<li>(110)2</li>
+				<li>(000)2</li>
+			</ul>
+		</div>
+		<p> <strong> Answer: (000)2 </strong></p>
+		<p class="p-0 m-0">
+		  Explanation: The binary numbers should comprise only two digits 0 and 1.
+          Also, for the base, the value should be 2 and it should be written as a subscript enclosing the entire number. Here, the fourth option gives the correct representation.	
+		</p>
+		- - - - - - - - - - - - - - - - 
+	</div>
+	
+</div>
+<hr>
+<br>
+
+
+
+
+
+
+
+
+<!-- DEMO TEM
+
+<div>
+	<h1>////////******* ********\\\\\\\\*** ***////</h1>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<hr>
+<br> 
+
+-->
+
+/////////////////////////////////////////////// END//////////////////////////////////////////////////////
+</div>
+</body>
+</html>
