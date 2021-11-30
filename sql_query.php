c4c457ea6c2c57a3ef45808516edfc2f612b4c6b
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-11-30 20:15:31 +0600

    sql query c10

--- sql_query.php
+++ sql_query.php
@@ -319,3 +319,29 @@ CREATE TABLE each_semister_result(
 UPDATE `registered_exam` JOIN course_year ON course_year.COURSE_YEAR_ID = registered_exam.COURSE_YEAR_ID JOIN programs ON programs.PROGRAMS_ID = course_year.PROGRAMS_ID SET registered_exam.course_code_on_of = 'SHOW' WHERE ( programs.PROGRAMS_ID = 12 OR programs.PROGRAMS_ID = 13 OR programs.PROGRAMS_ID = 14 OR programs.PROGRAMS_ID = 17 OR programs.PROGRAMS_ID = 18 OR programs.PROGRAMS_ID = 19)
 
 "
+    /*Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.*/
+
+
+"
+
+SELECT DISTINCT CITY FROM STATION WHERE ( NOT (CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%')) AND (NOT (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'));
+
+"
+
+/*Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.*/
+
+"
+ SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(Name, 3), ID ASC
+"
+
+/*Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.*/
+
+"
+SELECT name FROM Employee ORDER BY name ASC
+
+"
+/*Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.*/
+
+"
+SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id ASC
+"
