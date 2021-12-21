2b99896d12531105fdac9a9f20dca73b1bff362c
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2021-12-21 20:46:56 +0600

    multi sum

--- sql.php
+++ sql.php
@@ -222,5 +222,12 @@ FROM
 "
 SELECT b.N, CASE WHEN b.P is not null THEN CASE when (SELECT COUNT(*) FROM bst WHERE P = b.N) = 0 THEN 'Leaf' else 'Inner' END ELSE 'Root' END FROM bst b ORDER BY b.N;
 "
+"
+SELECT company_code, founder, 
+(SELECT COUNT(DISTINCT lead_manager_code) FROM lead_manager WHERE company_code = c.company_code), 
+(SELECT COUNT(DISTINCT senior_manager_code) FROM senior_manager WHERE company_code = c.company_code), 
+(SELECT COUNT(DISTINCT manager_code) FROM manager WHERE company_code = c.company_code), 
+(SELECT COUNT(DISTINCT employee_code) FROM employee WHERE company_code = c.company_code) FROM company c  ORDER BY company_code;
+"
 
 ?>
