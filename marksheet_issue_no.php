88e29cd6adb2372c9aa5a939457830d15c7f7d15
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2024-05-13 12:15:15 +0600

    CSE

--- marksheet_issue_no.php
+++ marksheet_issue_no.php
@@ -6,3 +6,7 @@ SELECT * FROM `registered_exam` WHERE `COURSE_YEAR_ID` BETWEEN 5 and 8
 //B.Sc. in Electrical and Electronics Engineering
 
 SELECT * FROM `registered_exam` WHERE `COURSE_YEAR_ID` BETWEEN 45 and 52
+
+// B.Sc. in Computer Science and Engineering
+
+SELECT * FROM `registered_exam` WHERE `COURSE_YEAR_ID` BETWEEN 53 and 60
