b61d0a495b96f7fcb0d049edad12de6b3bfcc966
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2024-05-30 14:22:11 +0600

    mbm

--- marksheet_issue_no.php
+++ marksheet_issue_no.php
@@ -14,3 +14,7 @@ SELECT * FROM `registered_exam` WHERE `COURSE_YEAR_ID` BETWEEN 45 and 52
 // B.Sc. in Computer Science and Engineering
 
 SELECT * FROM `registered_exam` WHERE `COURSE_YEAR_ID` BETWEEN 53 and 60
+
+// Masters in Bank Management
+
+SELECT * FROM `registered_exam` WHERE COURSE_YEAR_ID = 176 OR COURSE_YEAR_ID = 177 OR COURSE_YEAR_ID BETWEEN 206 and 208
