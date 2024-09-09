cc8281effbc528a5c8540ee7da4f41a55889bb85
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2024-09-09 11:39:33 +0600

    MSc in Physiotherapy

--- 0-marksheet_issue_no.php
+++ 0-marksheet_issue_no.php
@@ -122,5 +122,9 @@ SELECT * FROM `registered_exam` WHERE COURSE_YEAR_ID BETWEEN 65 and 68
 
 SELECT * FROM `registered_exam` WHERE COURSE_YEAR_ID BETWEEN 184 and 187
 
+// M.Sc. in Physiotherapy
+
+SELECT * FROM `registered_exam` WHERE COURSE_YEAR_ID BETWEEN 25 and 26
+
 
 
